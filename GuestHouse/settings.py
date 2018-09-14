fea633dae3fbca7811f0fdb66cfc2c2ff2ddadc2
Even <even311379@hotmail.com>
2018-09-14 16:21:23 +0800

    hot-fix! security issue

--- GuestHouse/settings.py
+++ GuestHouse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import MyImportantInfo as MIF
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,5 +134,5 @@ MEDIA_URL = '/media/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'even311379@gmail.com'
-EMAIL_HOST_PASSWORD = 'g121830443'
+EMAIL_HOST_USER = MIF.MyGmailAccount()
+EMAIL_HOST_PASSWORD = MIF.MyGmailPassword()
